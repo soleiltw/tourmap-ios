644cb8919f362946c4da1eacca53572af079f1e5
Edward Chiang <edwardubuntu@gmail.com>
2015-11-14 02:23:50 +0800

    Initial Commit

---
+++ TourMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TourMap
+//
+//  Created by Edward Chiang on 11/14/15.
+//  Copyright © 2015 Soleil Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
