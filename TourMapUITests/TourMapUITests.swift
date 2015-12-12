aa565660e1a9fb984c88f5f38bd8a4b8a4781a0a
Edward Chiang <edwardubuntu@gmail.com>
2015-12-12 13:11:59 +0800

    Integrate fastlane

--- TourMapUITests/TourMapUITests.swift
+++ TourMapUITests/TourMapUITests.swift
@@ -18,7 +18,24 @@ class TourMapUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        
+        /*
+
+        Open your Xcode project and make sure to do the following:
+        1) Add the ./SnapshotHelper.swift to your UI Test target
+        You can move the file anywhere you want
+        2) Call `setupSnapshot(app)` when launching your app
+        
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        
+        3) Add `snapshot("0Launch")` to wherever you want to create the screenshots
+        
+        */
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
